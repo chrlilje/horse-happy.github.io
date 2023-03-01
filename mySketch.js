6d981090eba1d8793a9dd0bc02b224b0a38b0d9c
Christian Liljedahl <christian@liljedahl.dk>
2023-03-01 18:22:43 +0000

    dd

---
+++ mySketch.js
@@ -0,0 +1,21 @@
+let myImg;
+function preload(){
+	myImg = loadImage("baum.png");	
+}
+
+let posx = 0;
+function setup() {
+	createCanvas(600, 400);
+	myImg.resize(200,0);
+	imageMode(CENTER);
+}
+
+function draw() {
+	background(100);
+	circle(posx, 200, 50);
+	image(myImg, posx, height/2);
+	posx += 5;
+	if(posx > width){
+		posx = 0;
+	}
+}
